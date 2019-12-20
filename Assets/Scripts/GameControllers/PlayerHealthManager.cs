6cdb199345deebad83e06ab2d977395bdfecbc69
Bhuvanesh <bhuvaneshwaran.m@hornbillfx.com>
2019-12-20 09:49:40 +0530

    GamePlayWithLooping

--- Assets/Scripts/GameControllers/PlayerHealthManager.cs
+++ Assets/Scripts/GameControllers/PlayerHealthManager.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
-public class PlayerHealthManager : MonoBehaviour
+public class PlayerHealthManager : MonoBehaviourPun, IPunObservable
 {
-    // Start is called before the first frame update
-    void Start()
+    public PhotonView pv;
+    public float currentHealth;
+    public float maxHealth;
+    public bool updateHealth;
+    public Image healthBar;
+
+    public void Update()
     {
-        
+        if (updateHealth)
+        {
+            healthBar.fillAmount = currentHealth / maxHealth;
+            updateHealth = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        
+        if (stream.IsWriting)
+        {
+            stream.SendNext(currentHealth);
+            stream.SendNext(maxHealth);
+        }
+        if (stream.IsReading)
+        {
+            currentHealth = (float)stream.ReceiveNext();
+            maxHealth = (float)stream.ReceiveNext();
+        }
     }
 }
